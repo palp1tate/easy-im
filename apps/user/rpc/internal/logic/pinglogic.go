021c08567ff4763502e68963e92ed93fa5aae51f
wxy <1939311091@qq.com>
2024-06-21 09:55:51 +0800

    feat: Initial commit

---
+++ apps/user/rpc/internal/logic/pinglogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"github.com/palp1tate/easy-im/apps/user/rpc/internal/svc"
+	"github.com/palp1tate/easy-im/apps/user/rpc/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type PingLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
+	return &PingLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *PingLogic) Ping(in *user.Request) (*user.Response, error) {
+	return &user.Response{
+		Pong: "easy-im by palp1tate",
+	}, nil
+}
