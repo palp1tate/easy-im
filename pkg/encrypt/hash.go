1ff4f1b59a61236a02977cc5b02b2512c1cb32fc
wxy <1939311091@qq.com>
2024-06-25 14:42:15 +0800

    fix: Remove MD5 hashing from hash.go
    The MD5 hashing function has been removed from the hash.go file. The corresponding import statements for 'crypto/md5' and 'encoding/hex' have also been eliminated, leaving only bcrypt importing and its corresponding function.

--- pkg/encrypt/hash.go
+++ pkg/encrypt/hash.go
@@ -1,17 +1,8 @@
 package encrypt
 
 import (
-	"crypto/md5"
-	"encoding/hex"
-
 	"golang.org/x/crypto/bcrypt"
 )
-
-func Md5(str []byte) string {
-	h := md5.New()
-	h.Write(str)
-	return hex.EncodeToString(h.Sum(nil))
-}
 
 // GenPasswordHash hash加密
 func GenPasswordHash(password []byte) ([]byte, error) {
